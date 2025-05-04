e3ed45afbb4ca93cbe1fe09aea31a9df43124da6
Mazen <mazenmagdy190.mm@gmail.com>
2025-05-04 14:05:47 +0300

    .

--- services/orderService.js
+++ services/orderService.js
@@ -1,74 +1,73 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET);
-const asyncHandler = require('express-async-handler');
-const factory = require('./handlersFactory');
-const ApiError = require('../utils/apiError');
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const asyncHandler = require("express-async-handler");
+const factory = require("./handlersFactory");
+const ApiError = require("../utils/apiError");
 
-const User = require('../models/userModel');
-const Product = require('../models/productModel');
-const Cart = require('../models/cartModel');
-const Order = require('../models/orderModel');
-
+const User = require("../models/userModel");
+const Product = require("../models/productModel");
+const Cart = require("../models/cartModel");
+const Order = require("../models/orderModel");
 
 // @desc    create cash order
 // @route   POST /api/v1/orders/cartId
 // @access  Protected/User
 exports.createCashOrder = asyncHandler(async (req, res, next) => {
-    // app settings
-    const taxPrice = 0;
-    const shippingPrice = 0;
+  // app settings
+  const taxPrice = 0;
+  const shippingPrice = 0;
 
-     // 1) Get cart depend on cartId
-const cart = await Cart.findById(req.params.cartId);
-if (!cart) {
+  // 1) Get cart depend on cartId
+  const cart = await Cart.findById(req.params.cartId);
+  if (!cart) {
     return next(
-    new ApiError(`There is no such cart with id ${req.params.cartId}`, 404)
+      new ApiError(`There is no such cart with id ${req.params.cartId}`, 404)
     );
-}
+  }
 
- // 2) Get order price depend on cart price "Check if coupon apply"
-const cartPrice = cart.totalPriceAfterDiscount
-? cart.totalPriceAfterDiscount
-: cart.totalCartPrice;
+  // 2) Get order price depend on cart price "Check if coupon apply"
+  const cartPrice = cart.totalPriceAfterDiscount
+    ? cart.totalPriceAfterDiscount
+    : cart.totalCartPrice;
 
-const totalOrderPrice = cartPrice + taxPrice + shippingPrice;
+  const totalOrderPrice = cartPrice + taxPrice + shippingPrice;
 
- // 3) Create order with default paymentMethodType cash
-const order = await Order.create({
+  // 3) Create order with default paymentMethodType cash
+  const order = await Order.create({
     user: req.user._id,
     cartItems: cart.cartItems,
     shippingAddress: req.body.shippingAddress,
     totalOrderPrice,
-});
+  });
 
   // 4) After creating order, decrement product quantity, increment product sold
-if (order) {
+  if (order) {
     const bulkOption = cart.cartItems.map((item) => ({
-    updateOne: {
+      updateOne: {
         filter: { _id: item.product },
         update: { $inc: { quantity: -item.quantity, sold: +item.quantity } },
-    },
+      },
     }));
     await Product.bulkWrite(bulkOption, {});
 
     // 5) Clear cart depend on cartId
     await Cart.findByIdAndDelete(req.params.cartId);
-}
+  }
 
-res.status(201).json({ status: 'success', data: order });
+  res.status(201).json({ status: "success", data: order });
 });
 
 exports.filterOrderForLoggedUser = asyncHandler(async (req, res, next) => {
-    if (req.user.role === 'user') req.filterObj = { user: req.user._id };
-    next();
+  if (req.user.role === "user") req.filterObj = { user: req.user._id };
+  next();
 });
-  // @desc    Get all orders
-  // @route   POST /api/v1/orders
-  // @access  Protected/User-Admin-Manager
+// @desc    Get all orders
+// @route   POST /api/v1/orders
+// @access  Protected/User-Admin-Manager
 exports.findAllOrders = factory.getAll(Order);
 
-  // @desc    Get all orders
-  // @route   POST /api/v1/orders
-  // @access  Protected/User-Admin-Manager
+// @desc    Get all orders
+// @route   POST /api/v1/orders
+// @access  Protected/User-Admin-Manager
 exports.findSpecificOrder = factory.getOne(Order);
 
 // @desc    Update order paid status to paid
@@ -91,8 +90,8 @@ exports.updateOrderToPaid = asyncHandler(async (req, res, next) => {
 
   const updatedOrder = await order.save();
 
-  res.status(200).json({ status: 'success', data: updatedOrder });
-}); 
+  res.status(200).json({ status: "success", data: updatedOrder });
+});
 
 // @desc    Update order delivered status
 // @route   PUT /api/v1/orders/:id/deliver
@@ -114,7 +113,7 @@ exports.updateOrderToDelivered = asyncHandler(async (req, res, next) => {
 
   const updatedOrder = await order.save();
 
-  res.status(200).json({ status: 'success', data: updatedOrder });
+  res.status(200).json({ status: "success", data: updatedOrder });
 });
 
 // @desc    Get checkout session from stripe and send it as response
@@ -140,27 +139,28 @@ exports.checkoutSession = asyncHandler(async (req, res, next) => {
 
   const totalOrderPrice = cartPrice + taxPrice + shippingPrice;
 
-   // 3) Create stripe checkout session
+  // 3) Create stripe checkout session
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
         price_data: {
-          currency: 'egp',
+          currency: "egp",
           product_data: {
-        name: req.user.name,
+            name: req.user.name,
           },
-         unit_amount: totalOrderPrice * 100,
+          unit_amount: totalOrderPrice * 100,
+        },
+        quantity: 1,
       },
-      quantity: 1,
-  }],
-    mode: 'payment',
-    success_url: `${req.protocol}://${req.get('host')}/orders`,
-    cancel_url: `${req.protocol}://${req.get('host')}/cart`,
+    ],
+    mode: "payment",
+    success_url: `${req.protocol}://${req.get("host")}/orders`,
+    cancel_url: `${req.protocol}://${req.get("host")}/cart`,
     customer_email: req.user.email,
     client_reference_id: req.params.cartId,
     metadata: req.body.shippingAddress,
   });
 
   // 4) send session to response
-  res.status(200).json({ status: 'success', session });
+  res.status(200).json({ status: "success", session });
 });
